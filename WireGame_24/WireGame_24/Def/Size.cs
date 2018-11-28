db5f017bef44350599ffb28ff028e32a13a4376d
ri_rino <orino_chan@outlook.jp>
2018-11-28 15:13:34 +0900

    スクロール処理、カメラクラスの追加

---
+++ WireGame_24/WireGame_24/Def/Size.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WireGame_24.Def
+{
+    class Size
+    {
+        public static readonly int BlockX = 64;
+        public static readonly int BlockY = 64;
+
+    }
+}
---
+++ WireGame_24/WireGame_24/Scene/Score.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using WireGame_24.Device;
+
+namespace WireGame_24.Scene
+{
+    class Score
+    {
+        private int score;
+        private int poolScore;
+
+        public Score()
+        {
+            Initialize();
+        }
+        public void Initialize()
+        {
+            score = 0;
+            poolScore = 0;
+        }
+        public void Update(GameTime gameTime)
+        {
+            if (poolScore > 0)
+            {
+                score += 1;
+                poolScore -= 1;
+            }
+            else if (poolScore < 0)
+            {
+                score -= 1;
+                poolScore += 1;
+            }
+        }
+        public void Add()
+        {
+            poolScore += 1;
+        }
+        public void Add(int num)
+        {
+            poolScore += num;
+        }
+        public void Draw(Renderer renderer)
+        {
+            renderer.DrawTexture("score", new Vector2(50, 10));
+            renderer.DrawNumber("number", new Vector2(250, 13), score);
+        }
+        public int GetScore()
+        {
+            int currentScore = score + poolScore;
+            if (currentScore < 0)
+            {
+                currentScore = 0;
+            }
+            return currentScore;
+        }
+        public void Shutdown()
+        {
+            score += poolScore;
+            if (score < 0)
+            {
+                score = 0;
+            }
+
+            poolScore = 0;
+        }
+    }
+}

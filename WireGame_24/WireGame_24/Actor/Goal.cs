224900eedf69efb38191609754608bb45448910f
syake <->
2018-11-29 15:49:59 +0900

    Goalをプロジェクトに追加

--- WireGame_24/WireGame_24/Actor/Goal.cs
+++ WireGame_24/WireGame_24/Actor/Goal.cs
@@ -8,28 +8,28 @@ using WireGame_24.Device;
 
 namespace WireGame_24.Actor
 {
-    class Goal : GameObject
+    class Goal// : GameObject
     {
-        public Goal(Vector2 position,GameDevice gameDevice):base("",position,32,32,gameDevice)
-        {
-        }
-        public Goal(Goal other) : this(other.position, other.gameDevice)
-        {
-        }
-        public override object Clone()
-        {
-            return new Goal(this);
-        }
+        //public Goal(Vector2 position,GameDevice gameDevice):base("",position,32,32,gameDevice)
+        //{
+        //}
+        //public Goal(Goal other) : this(other.position, other.gameDevice)
+        //{
+        //}
+        //public override object Clone()
+        //{
+        //    return new Goal(this);
+        //}
 
-        public override void Hit(GameObject gameObject)
-        {
-        }
+        //public override void Hit(GameObject gameObject)
+        //{
+        //}
 
-        public override void Update(GameTime gameTime)
-        {
-        }
-        public override void Draw(Renderer renderer)
-        {
-        }
+        //public override void Update(GameTime gameTime)
+        //{
+        //}
+        //public override void Draw(Renderer renderer)
+        //{
+        //}
     }
 }

aa0a549a2a782b7c99fa7a10915b91ece1ab52df
hiro12 <hirokurospeed@gmail.com>
2018-12-06 15:24:59 +0900

    ブロッククラス、マップクラス、スペースクラス作成

---
+++ WireGame_24/WireGame_24/Actor/Block.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using WireGame_24.Device;
+
+namespace WireGame_24.Actor
+{
+    class Block : GameObject
+    {
+        public Block(Vector2 position, GameDevice gameDevice)
+            : base("block", position, 32, 32, gameDevice)
+        { }
+
+        public Block(Block other)
+            : this(other.position, other.gameDevice)
+        {
+        }
+
+        public override object Clone()
+        {
+            return new Block(this);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        public override void Hit(GameObject gameObject)
+        {
+        }
+    }
+}

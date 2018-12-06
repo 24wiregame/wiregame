0290a13110bf8d830751dfe3008b8a28d0b9ff81
hiro12 <hirokurospeed@gmail.com>
2018-12-06 15:08:39 +0900

    stage001csv追加 Screen縦1920横1080に変更

--- WireGame_24/WireGame_24/Def/Screen.cs
+++ WireGame_24/WireGame_24/Def/Screen.cs
@@ -11,7 +11,7 @@ namespace WireGame_24.Def
     /// </summary>
     static class Screen
     {
-        public static readonly int Width = 2000;//画面横幅
-        public static readonly int Height = 1000;//画面縦幅
+        public static readonly int Width = 1920;//画面横幅
+        public static readonly int Height = 1080;//画面縦幅
     }
 }
